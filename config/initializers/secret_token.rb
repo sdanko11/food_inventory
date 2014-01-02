ea354e1fe52727ba25a0335c41fe772daba2bc3d
steve <stevedanko1@gmail.com>
2014-01-01 21:27:50 -0500

    set up gem file and enviorment

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FoodInventory::Application.config.secret_key_base = '7ae41ee6c9871d45335db75f81d287431041dc3be8cceaf1d7ffc185baad1184d2863dd5063bc2884ddcd30b4ee07b405cbcf972309d65a2eeb3a45e35317f2c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+FoodInventory::Application.config.session_store :cookie_store, key: '_food_inventory_session'
